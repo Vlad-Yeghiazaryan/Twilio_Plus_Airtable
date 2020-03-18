fa96a10aa479651b9bdfd86c708efe2b12b37e84
Vlad-Yeghiazaryan <vladimir.y2000@gmail.com>
2020-03-19 03:07:47 +0400

    final commit

--- AirtableAPI_Receive.js
+++ AirtableAPI_Receive.js
@@ -1,45 +1,44 @@
 // https://www.twilio.com/console/functions/manage
-exports.handler = function(context, event, callback) {
-    
-  // Airtable_API_Key = Available in the Account page 
+exports.handler = function (context, event, callback) {
+  // Airtable_API_Key = Available in the Account page
   // baseId = Available in https://airtable.com/api (make sure you are logged in)
-  
-  var Airtable = require('airtable');
-  var base = new Airtable({apiKey: context.Airtable_API_Key}).base(context.baseId);
-  let response = new Twilio.Response();
 
-// Build list of headers
-let headers = {
-  "Access-Control-Allow-Origin": "*", // Restricts access to specific urls
-  "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type"
-};
+  var Airtable = require('airtable')
+  var base = new Airtable({ apiKey: context.Airtable_API_Key }).base(context.baseId)
+  const response = new Twilio.Response()
 
-// Set headers in response
-response.setHeaders(headers);
+  // Build list of headers
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Restricts access to specific urls
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  }
 
-// Data that will be sent to the database
-  let member = {
-      Name: event.name,
-      Url: event.url,
-      "Date": Date.now()
-  };
-  
+  // Set headers in response
+  response.setHeaders(headers)
+
+  // Data that will be sent to the database
+  const member = {
+    Name: event.name,
+    Url: event.url,
+    Date: Date.now()
+  }
+
   // Database required input format
   base('Members').create([
-{ 
-  "fields": member
-}], 
-// In case error occurs
-function(err, records) {
+    {
+      fields: member
+    }],
+  // In case error occurs
+  function (err, records) {
     if (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
     records.forEach(function (record) {
-        // Error log and response log
-        console.log(record.getId());
-        callback(null, response);
-    });
-  });
-};
+      // Error log and response log
+      console.log(record.getId())
+      callback(null, response)
+    })
+  })
+}
--- AirtableAPI_Send.js
+++ AirtableAPI_Send.js
@@ -1,10 +1,10 @@
-const btn = document.getElementById("btn")
+const btn = document.getElementById('btn')
 
 const fetchApi = async (fn, ln) => {
   const rawResponse = await fetch('https://capri-donkey-6355.twil.io/myapiend', {
     method: 'POST',
     headers: {
-      "Content-Type": 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(
       {
@@ -16,8 +16,8 @@ const fetchApi = async (fn, ln) => {
   const content = await rawResponse
   console.log(content)
 }
-btn.addEventListener("click", async () => {
-  let firstname = document.getElementById("fn").value
-  let lastname = document.getElementById("ln").value
+btn.addEventListener('click', async () => {
+  const firstname = document.getElementById('fn').value
+  const lastname = document.getElementById('ln').value
   fetchApi(firstname, lastname)
-});
+})
